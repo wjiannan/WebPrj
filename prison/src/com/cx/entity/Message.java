b5621f66b13f7945e0abe239fec5092cb9044c05
SmokeCat <smokecat@gmail.com>
2018-12-03 21:16:23 +0800

    05 添加实体类和数据库脚本

---
+++ prison/src/com/cx/entity/Message.java
@@ -0,0 +1,75 @@
+package com.cx.entity;
+
+import java.util.Date;
+
+/*
+ * 实体类
+ * 	id number(5),
+	sender_id number(5),
+	recipient_id number(5),
+	content varchar2(1000),
+	create_time date DEFAULT SYSDATE,
+	constraint tbl_message_id_pk primary key(id),
+	constraint tbl_message_recipient_nn check(recipient_id is not null)
+
+ */
+
+public class Message {
+	private Integer id;
+	private Integer SenderId;
+	private Integer RecipientId;
+	private String content;
+	private Date createTime;
+	
+	public Message() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getSenderId() {
+		return SenderId;
+	}
+
+	public void setSenderId(Integer senderId) {
+		SenderId = senderId;
+	}
+
+	public Integer getRecipientId() {
+		return RecipientId;
+	}
+
+	public void setRecipientId(Integer recipientId) {
+		RecipientId = recipientId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [id=" + id + ", SenderId=" + SenderId + ", RecipientId=" + RecipientId + ", content=" + content
+				+ ", createTime=" + createTime + "]";
+	}
+	
+	
+}
